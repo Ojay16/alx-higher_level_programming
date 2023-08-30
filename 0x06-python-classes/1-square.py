d4dec9151ce498171321f991bf48c33170318524
Ojay16 <juliusjeff16@gmail.com>
2023-08-30 00:01:38 +0000

    updated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" difines class Square."""
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new Square.
+        Args:
+            size (int): size of new square.
+        """
+        self.__size = size
