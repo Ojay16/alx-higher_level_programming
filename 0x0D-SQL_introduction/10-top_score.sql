d55dcb6127cc93a4cba37e039f772885ea3b1b07
Ojay16 <juliusjeff16@gmail.com>
2023-10-18 19:58:17 -0700

    updated

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Thos script lists all the rows of table second_table
+-- from database hbtn_0c_0
+-- results display score and name ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC;
