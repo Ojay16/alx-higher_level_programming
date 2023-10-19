6d7f1c2ec01656fa644deac8026cfb876cf71472
Ojay16 <juliusjeff16@gmail.com>
2023-10-18 20:00:28 -0700

    updated

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- This script lists all the rows of  table second_table
+-- from database hbtn_0c_0
+-- results display score nd name ordered by score
+-- with score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
