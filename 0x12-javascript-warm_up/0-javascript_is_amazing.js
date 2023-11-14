d9b5f1000a485a9d1df207e95278f36aa4075f50
Ojay16 <juliusjeff16@gmail.com>
2023-11-13 16:31:12 -0800

    firt commit

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Script that prints "JavaScript is amazing".
+// Script prints "JavaScript is amazing" to console.
 
 const myVar =  'JavaScript is amazing';
 console.log(myVar);
