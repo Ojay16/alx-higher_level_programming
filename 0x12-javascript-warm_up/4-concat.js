18904d8ba4ce73c181af78eda63f1d38564a819e
Dark-code16 <d.a.r.k.p.o.e.t.jay@gmail.com>
2024-04-22 17:40:28 -0700

    upda5ed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-//script prints two arguments passed to it, in the following format: “is”
+// script prints two arguments passed to it, in the following format: “is”
 
 console.log(`${process.argv[2]} is ${process.argv[3]}`);
