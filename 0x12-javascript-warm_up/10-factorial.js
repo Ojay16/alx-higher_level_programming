28f591aea0fd728826676cf29f477925f18d8a37
Dark-code16 <d.a.r.k.p.o.e.t.jay@gmail.com>
2024-04-22 17:51:37 -0700

    updated

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// This script prints and computes a factorial.
+
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+}
+
+console.log(factorial(Number(process.argv[2])));i
